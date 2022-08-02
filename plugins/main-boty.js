3cc61ed43df9990d25d1934d6d38c1feecc947b4
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-02 23:05:28 +0800

    Update main-boty.js

--- plugins/main-boty.js
+++ plugins/main-boty.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "Amirr", author: "@gaara_xploite" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "Amirr", author: "@amirrwibuu_01" })
 }
 
 handler.customPrefix = /^(boti|boty|boty binal)$/i
