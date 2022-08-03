5c836b18ce74b071bc6f84790fa977be7ef2f86d
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-03 19:54:31 +0800

    Update main-huuu.js

--- plugins/main-huuu.js
+++ plugins/main-huuu.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "Gaara Ofc", author: "@gaara_xploite" })
+conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "Amirr", author: "@amirrwibuu_01" })
 }
 
 handler.customPrefix = /^(huuu)$/i
