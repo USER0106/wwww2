1e6f78958300f750882f55ab70e92bc565b1aab4
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-03 20:03:51 +0800

    Update main-nangis.js

--- plugins/main-nangis.js
+++ plugins/main-nangis.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/44c08e07a1c81411d5a69.jpg', m, { packname: "gaarabotz", author: "@gaaraxploite" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/44c08e07a1c81411d5a69.jpg', m, { packname: "Amerr", author: "@amirrwibuu_01" })
 }
 
 handler.customPrefix = /^(😭|😭😭|😭😭😭|😭😭😭😭|😭😭😭😭😭|😭😭😭😭😭😭)$/i
