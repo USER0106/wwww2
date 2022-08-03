7b3831a28500153abe5ce3591e6ae0fb49a55eb8
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-03 20:09:06 +0800

    Update main-nani.js

--- plugins/main-nani.js
+++ plugins/main-nani.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/133d80c6c852427d7a7ff.png', m, { packname: "gaarabotz", author: "@gaaraxploite" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/133d80c6c852427d7a7ff.png', m, { packname: "Amerr", author: "@amirrwibuu_01" })
 }
 
 handler.customPrefix = /^(nani|tes|tess|test|wibu)$/i
